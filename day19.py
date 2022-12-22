1ec2d283dc1317909f959fc41b12100c83fa94e8
Adrianus Kleemans <a.kleemans@gmail.com>
2022-12-22 19:31:56 +0100

    Day 19, pt. 2

--- day19.py
+++ day19.py
@@ -1,5 +1,4 @@
 import operator
-import time
 from typing import List, Dict
 
 # Day 19: Not Enough Minerals
@@ -47,20 +46,6 @@ class State:
                 options.append(idx)
         return options[::-1]
 
-        # Don't buy more robots than the highest resource cost
-        max_cost = [0, 0, 0, 0]
-        for recipe in recipes.values():
-            for i in range(4):
-                max_cost[i] = max(max_cost[i], recipe[i])
-        # print('max_cost:', max_cost)
-        # input()
-        options2 = []
-        for i in options:
-            if self.robots[i] < max_cost[i]:  # or self.resources[i] >= max_cost[i]:
-                options2.append(i)
-
-        return options2[::-1]
-
     def buy_robot(self, robot_type: int, recipe: List[int]) -> 'State':
         resources = sub(self.resources, recipe)
         robots = [*self.robots]
@@ -82,38 +67,17 @@ class State:
             max_reachable += self.robots[3] + i
         return max_reachable
 
-    def should_buy(self, recipes) -> bool:
-        max_cost = max(recipes[0][0], recipes[1][0])
-        buy_options = self.buy_options(recipes)
-        # Start: if either ore or clay robot *can* be bought, one *must* be bought
-        # if all(r == 0 for r in self.robots[1:]) and self.resources[0] >= max_cost:
-        #    return True
-
-        # If everything *can* be bought, something *must* be bought
-        # if len(buy_options) == 4:
-        #    return True
-
-        # If waiting doesn't provide new kind of robot, must buy something now
-        future_resources = add(self.resources, [r * 25 for r in self.robots])
-        if len(buy_options) > 0 and buy_options == self.buy_options(recipes, future_resources):
-            return True
-
-        return False
-
     def __str__(self):
         return f'State t={self.t} resources={self.resources}, robots={self.robots}'
+
+    def hash(self):
+        return f'{self.t}{self.resources}{self.robots}'
 
 
 class Blueprint:
     def __init__(self, idx, recipes: Dict[int, List[int]]):
         self.idx = idx
         self.recipes = recipes
-
-    def __str__(self):
-        return str(self.recipes)
-
-    def __repr__(self):
-        return self.__str__()
 
 
 def get_blueprints(data) -> List[Blueprint]:
@@ -150,80 +114,82 @@ def solve(blueprint: Blueprint, t: int) -> int:
     for recipe in recipes.values():
         for i in range(4):
             max_costs[i] = max(max_costs[i], recipe[i])
-    # print('max_costs:', max_costs)
 
     already_seen = set()
-    count = 0
+    # count = 0
     highest_geodes = -1
     reachable_high = 0
+    # t = time.time()
     while len(queue) > 0:
-        count += 1
+        # count += 1
         state = queue.pop()
-        if str(state) in already_seen:
+        if state.hash() in already_seen:
             continue
         else:
-            already_seen.add(str(state))
+            already_seen.add(state.hash())
 
-        if count % 100_000 == 0:
-            print('Queue:', len(queue), 'state:', state)
+        # if count % 100_000 == 0:
+        #     print('Queue:', len(queue), 'state:', state, 't=', time.time() - t)
 
         if state.t == 0:
             if state.get_geodes() > highest_geodes:
                 highest_geodes = state.get_geodes()
-                print('New high:', highest_geodes, 'state:', state)
+                # print('New high:', highest_geodes, 'state:', state)
                 reachable_high = highest_geodes
             continue
-
-        # Start earlier with obsidian / geode robots
-        # TODO remove, should be possible with other optimizations
-        # if state.t < 12 and state.robots[2] == 0 or state.t < 6 and state.robots[3] == 0:
-        #    continue
 
         reachable_high = max(reachable_high, state.min_geodes())
         if reachable_high > state.max_reachable_geodes():
             continue
 
-        # Check if resources in range
-        # cont = False
-        # for i in range(4):
-        #    if state.resources[i] > max_costs[i] * 4:
-        #        cont = True
-        #        break
-        # if cont:
-        #    continue
-
         buy_options = state.buy_options(recipes)
-        # print(f'State {state}, buy options', buy_options)
-        # input()
 
         for buy_option in buy_options:
+            if buy_option < 3 and state.robots[buy_option] >= max_costs[buy_option]:
+                continue
+            if state.robots[2] > 0 and buy_option == 0:
+                continue
             new_state = state.buy_robot(buy_option, recipes[buy_option])
             new_state.tick()
             queue.append(new_state)
 
-        # Do nothing and tick()
-        if not state.should_buy(recipes):
+        # If waiting doesn't provide new kind of robot, must buy something now
+        future_resources = add(state.resources, [r * 25 for r in state.robots])
+        if len(buy_options) > 0 and buy_options == state.buy_options(recipes, future_resources):
+            continue
+        else:
             state.tick()
             queue.append(state)
 
-    return blueprint.idx * highest_geodes
+    return highest_geodes
 
 
 def part1(data: List[str]):
     blueprints = get_blueprints(data)
 
     total_quality_level = 0
-    t = time.time()
+    # t = time.time()
     for blueprint in blueprints:
-        total_quality_level += solve(blueprint, 24)
+        # print('Solving blueprint', blueprint.idx)
+        total_quality_level += blueprint.idx * solve(blueprint, 24)
 
-    print('Time for both blueprints:', time.time() - t)
+    # print('Time for all blueprints:', time.time() - t)
+    # Initial time: 203s
+    # With robots max cost: 19.2s
+    # With simple robot arch: 12s
 
     return total_quality_level
 
 
 def part2(data: List[str]):
-    return 1
+    blueprints = get_blueprints(data)
+    m = 1
+    # t = time.time()
+    for blueprint in blueprints[:3]:
+        # print('Solving blueprint', blueprint.idx)
+        m *= solve(blueprint, 32)
+        # print('m=', m, 'total t=', time.time() - t)
+    return m
 
 
 def main():
@@ -232,15 +198,13 @@ def main():
 
     part1_test_result = part1(test_data)
     assert part1_test_result == 33, f'Part 1 test input returned {part1_test_result}'
-    return
     part1_result = part1(data)
     assert part1_result == 817, f'Part 1 returned {part1_result}'
 
-    part2_test_result = part2(test_data)
-    assert part2_test_result == 0, f'Part 2 test input returned {part2_test_result}'
+    part2_test_result = part2(test_data)  # 3472 = 63*56
+    assert part2_test_result == 3472, f'Part 2 test input returned {part2_test_result}'
     part2_result = part2(data)
-    print('Part 2:', part2_result)  # remove
-    assert part2_result == 0, f'Part 2 returned {part2_result}'
+    assert part2_result == 4216, f'Part 2 returned {part2_result}'
 
 
 if __name__ == '__main__':
